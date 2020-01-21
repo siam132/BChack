71bd7eb758acc0706a7f563bafdc4fa2915d0dfb
Shahidullah <shahidullah.rahman223@gmail.com>
2020-01-21 15:24:53 -0500

    Added responsiveness to FAQ and Schedule

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -7,20 +7,19 @@ import {
   AccordionItemButton,
   AccordionItemPanel
 } from "react-accessible-accordion";
-import 'react-accessible-accordion/dist/fancy-example.css';
-
+import "react-accessible-accordion/dist/fancy-example.css";
 
 function FAQ() {
   return (
-    <div className="row" id="faq">
-      <h1>FAQ</h1>
-      <div className="col-8 py-4">
-      {FAQData.map(e => {
-        return <FaqRenderer question={e.Question} answer={e.Answer} />;
-      })}
-      </div>
-      <div className="col-4 ">
-
+    <div className="container-fluid">
+      <div className="row" id="faq">
+        <h1>FAQ</h1>
+        <div className="col-lg-8 col-md-12 col-sm-12 col-xs-12 py-4">
+          {FAQData.map(e => {
+            return <FaqRenderer question={e.Question} answer={e.Answer} />;
+          })}
+        </div>
+        
       </div>
     </div>
   );
@@ -31,7 +30,9 @@ function FaqRenderer(props) {
     <Accordion allowZeroExpanded={"true"}>
       <AccordionItem>
         <AccordionItemHeading className="accordion__heading">
-          <AccordionItemButton className="h3 py-4 accordion__button">{props.question}</AccordionItemButton>
+          <AccordionItemButton className="h3 py-4 accordion__button">
+            {props.question}
+          </AccordionItemButton>
         </AccordionItemHeading>
         <AccordionItemPanel className="haccordion__panel h4">
           <p>{props.answer}</p>

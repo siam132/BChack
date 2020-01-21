cf65f9d055c3398272c386c28b3ca966e900beef
Shahzoda Davlatova <shahzoda01@gmail.com>
2020-01-21 16:18:46 -0500

    scheduling format

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,43 +1,41 @@
 import React from "react";
-import Clock from "react-live-clock";
-
 import scheduleData from "../data/Schedule";
 
 function Schedule() {
   return (
-    
-      <div id="schedule" className="container-fluid">
+    <div id="schedule">
         <div className="row">
-          <div className="col-4 col-md-4 col-sm-4 py-5">
-          </div>
-          <div className="col-7 col-md-7 col-sm-6 py-3">
-            <h1 className="py-4 text-warning rounded">Schedule</h1>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col"> Time</th>
-                  <th scope="col">Acitvity</th>
-                </tr>
-              </thead>
-              {scheduleData.map(e => {
-                return <ActivityRenderer time={e.time} activity={e.activity} />;
-              })}
-            </table>
-          </div>
+        <div className="col-12 text-center">
+          <h1>Schedule</h1>
+        </div>
+        </div>
+        <div className="row">
+        <div className="col-6 schedule col-sm-12">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col-6"> Time</th>
+                <th scope="col-6">Acitvity</th>
+              </tr>
+            </thead>
+            <tbody>
+            {scheduleData.map(e => {
+              return <ActivityRenderer time={e.time} activity={e.activity} />;
+            })}
+            </tbody>
+          </table>
         </div>
       </div>
-
+    </div>
   );
 }
 
 function ActivityRenderer(props) {
   return (
-    <tbody>
       <tr>
         <td>{props.time}</td>
         <td>{props.activity}</td>
       </tr>
-    </tbody>
   );
 }
 

bb58b08448b18df58d5ee4de16c5be3b2193ebe1
Shahzoda Davlatova <shahzoda01@gmail.com>
2020-01-23 22:00:43 -0500

    refactored contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,60 +6,35 @@ import MWEI from "../Logos/MWEILogo.png";
 function Contacts() {
   return (
     <div id="contact">
-    
       {/* TO DO: MAKE THE SLIDE HAPPEN WHEN THE ORGANIZERS SECTION IS VIEWED */}
       <div className="slide-left">
         <h1>Meet the Organizers!</h1>
       </div>
 
-        <div className="container-fluid">
-          <div className="row" style={{padding: 30}}>
-
-            <div className = "col-4" align="center">
-            <img src={ CS } className="logo cs" />
-
-              <br />
-              <p>
-                <b>Computer Science Club </b>
-                <br />
-                Founders of Hack Brooklyn, got together website, sponsors, clubs and everything in the mix
-                <br />
-                {/* TO DO: remove automatic line break */}
-
-
-                <a href="https://www.instagram.com/bc_compsci/"><i class="fab fa-instagram"></i></a>
-                <a href="https://www.facebook.com/groups/bc.cis.club/"><i class="fab fa-facebook"></i></a>
-              </p>
-            </div>
-
-            <div className="col-4" align="center">
-
-              <img src={WICS} className="wics logo" />
-              <br />
-              <p><b>Women in Computer Science Club </b>
-                <br />
-                Founders of Hack Brooklyn, got together website, sponsors, clubs and everything in the mix
-                <br />
-                <a href="https://www.linkedin.com/in/wics-brooklyn-college-70643b192/"><i class="fab fa-linkedin"></i></a>
-                <a href="https://www.instagram.com/bc_wics_/"><i class="fab fa-instagram"></i></a>
-                <a href="https://www.facebook.com/BCWomenInCS/"><i class="fab fa-facebook"></i></a>
-              </p>
-            </div>
-
-            <div className = "col-4" align="center">
-              <img src={MWEI} className="MWEI logo" />
-              <br />
-              <p><b>Muslim Women's Education Initiative</b>
-                <br />
-                Helping Hands on Hack Day for a Seamless Hack Experience
-                <br />
-                <a href="https://www.instagram.com/explore/locations/101722999919422/brooklyn-college-islamic-society-muslim-womens-educational-initiative/"><i class="fab fa-instagram"></i></a>
-                <a href="https://www.facebook.com/BCISO/"><i class="fab fa-facebook"></i></a>
-              </p>
-            </div>
-
-          </div>
+      <div className="row" style={{ padding: 30 }}>
+        <div className="col-4" align="center">
+          <img src={CS} className="logo cs" />
+          <p><b>Computer Science Club </b></p>
+          <p>Founders of Hack Brooklyn, got together website, sponsors, clubs and everything in the mix.</p>
+          <a href="https://www.instagram.com/bc_compsci/"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.facebook.com/groups/bc.cis.club/"><i class="fab fa-facebook"></i></a>
         </div>
+        <div className="col-4" align="center">
+          <img src={WICS} className="wics logo" />
+          <p><b>Women in Computer Science Club </b></p>
+          <p>Founders of Hack Brooklyn, got together website, sponsors, clubs and everything in the mix</p>
+          <a href="https://www.linkedin.com/in/wics-brooklyn-college-70643b192/"><i class="fab fa-linkedin"></i></a>
+          <a href="https://www.instagram.com/bc_wics_/"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.facebook.com/BCWomenInCS/"><i class="fab fa-facebook"></i></a>
+        </div>
+        <div className="col-4" align="center">
+          <img src={MWEI} className="MWEI logo" />
+          <p><b>Muslim Women's Education Initiative</b></p>
+          <p>Helping Hands on Hack Day for a Seamless Hack Experience</p>
+          <a href="https://www.instagram.com/explore/locations/101722999919422/brooklyn-college-islamic-society-muslim-womens-educational-initiative/"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.facebook.com/BCISO/"><i class="fab fa-facebook"></i></a>
+        </div>
+      </div>
     </div>
   );
 }

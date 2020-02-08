027bbc94be4879c21e48ed280a7f277289e2935b
KevinTamCS <kevin@kevintam.dev>
2020-02-07 22:50:31 -0500

    Fix linter warnings

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -27,11 +27,11 @@ function Landing() {
             <p>
             {/* ❤We love women, non-binary and LGBTQ❤ ️ */}
             </p>
-            <p>⏰ March 29th </p>
+            <p>March 29th </p>
             {/* link to location */}
             <p>
               <a className="locater" href="https://www.google.com/maps/place/Brooklyn+College/@40.6304648,-73.9543017,17z/data=!4m5!3m4!1s0x89c244b4f418b1cd:0xb1d4b115f28f8e17!8m2!3d40.6302494!4d-73.9553609">
-                📍Brooklyn College
+                Brooklyn College
               </a>
             </p>   
           </div>

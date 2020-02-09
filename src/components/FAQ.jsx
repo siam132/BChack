d58df8a10fe44ab3fb16344d193eb911c7fe49f5
Kevin Tam <kevin@kevintam.dev>
2020-02-08 22:32:07 -0500

    Update FAQ to closely resemble prototype (#28)

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,47 +1,36 @@
+// @ts-check
 import React from "react";
 import FAQData from "../data/FAQ";
-import {
-  Accordion,
-  AccordionItem,
-  AccordionItemHeading,
-  AccordionItemButton,
-  AccordionItemPanel
-} from "react-accessible-accordion";
 
-
-function FAQ() {
+function FaqQuestion(props) {
   return (
-
-    <div className="container-fluid">
-      <div className="row" id="faq">
-        <h1 className="section_title">FAQ</h1>
-        <div className="col-12">
-          {FAQData.map(e => {
-            return <FaqRenderer question={e.Question} answer={e.Answer} />;
-          })}
-        </div>
-        
-
-
-      </div>
+    <div className="faq-q-and-a">
+      <h3 className="faq-question">
+        {FAQData[props.questionNumber].Question}
+      </h3>
+      <p className="faq-answer">
+        {FAQData[props.questionNumber].Answer}
+      </p>
     </div>
   );
 }
 
-function FaqRenderer(props) {
+function FAQ() {
   return (
-    <Accordion allowZeroExpanded={"true"}>
-      <AccordionItem>
-        <AccordionItemHeading className="accordion__heading">
-          <AccordionItemButton className="h3 py-4 accordion__button">
-            {props.question}
-          </AccordionItemButton>
-        </AccordionItemHeading>
-        <AccordionItemPanel className="haccordion__panel h4">
-          <p>{props.answer}</p>
-        </AccordionItemPanel>
-      </AccordionItem>
-    </Accordion>
+    <div className="container-fluid">
+      <div className="row" id="faq">
+        <h1 className="faq-title">Frequently Asked Questions</h1>
+          <div className="col-sm left-column" >
+            <FaqQuestion questionNumber={0} />
+            <FaqQuestion questionNumber={2} />
+            <FaqQuestion questionNumber={4} />
+          </div>
+          <div className="col-sm right-column">
+            <FaqQuestion questionNumber={1} />
+            <FaqQuestion questionNumber={3} />
+          </div>
+      </div>
+    </div>
   );
 }
 

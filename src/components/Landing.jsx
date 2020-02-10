1c010eb5c4d1d29eb43a56a4ae338995842124cf
Shahzoda Davlatova <shahzoda01@gmail.com>
2020-02-10 02:15:44 -0500

    removed exra comments that are not helpful.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,28 +1,16 @@
 import React from "react";
-// import styled, {keyframes} from 'styled-components';
-// import {fadeIn} from 'react-animations';
 import Zoom from 'react-reveal/Zoom';
 import Countdown from 'react-countdown';
 
-// const FadeIn = styled.div`animation: 3s ${keyframes`${fadeIn}`} `;
 
 function Landing() {
   return (
     <div id="landing" className="row">
-      {/* <FadeIn> */}
         <div className="col-12">
           <Zoom cascade duration={3000}><p className="intro">Where Your Fantasies Come Alive</p></Zoom>
           <p><br /></p>
           <h1 className="glitch">HACK <br /> Brooklyn</h1>
-          {/* <Typical
-              steps={['', 1000, 'Brooklyn', 1000]}
-              loop={Infinity}
-              wrapper="h1"
-              className="glitch"
-            />  */}
-
           <p>
-            {/* ❤We love women, non-binary and LGBTQ❤ ️ */}
           </p>
           <p>March 29th </p>
           {/* link to location */}
@@ -43,10 +31,6 @@ function Landing() {
               }
             }}
           />
-
-        {/* EMBED GOOGLE MAPS */}
-        {/* get an API key and attach it in the URL to build iframe :) */}
-        {/* <iframe class="map" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/place/Brooklyn+College/@40.6310931,-73.956692,17.54z/data=!4m5!3m4!1s0x89c244b4f418b1cd:0xb1d4b115f28f8e17!8m2!3d40.6302494!4d-73.9553609"></iframe> */}
       </div>
 
       {/* button to typeform */}
@@ -55,13 +39,8 @@ function Landing() {
         <div className="bttn-group">
           <button href="#" className="btn btn-danger mx-2 px-5 ">Register</button>
           <button href="#" className="btn btn-success mx-2 px-5">Volunteer</button>
-
-
         </div>
-
       </div>
-
-      {/* </FadeIn> */}
     </div>
   );
 }

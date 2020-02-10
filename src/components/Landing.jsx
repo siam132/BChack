5b50462a1148cec3445eaa5a78432792fdaf559f
Kevin Tam <kevin@kevintam.dev>
2020-02-09 21:54:27 -0500

    Add countdown timer (#34)
    * Add countdown timer
    
    * Update NPM package.json

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 // import styled, {keyframes} from 'styled-components';
 // import {fadeIn} from 'react-animations';
 import Zoom from 'react-reveal/Zoom';
+import Countdown from 'react-countdown';
 
 // const FadeIn = styled.div`animation: 3s ${keyframes`${fadeIn}`} `;
 
@@ -33,7 +34,19 @@ function Landing() {
               <a className="locater" href="https://www.google.com/maps/place/Brooklyn+College/@40.6304648,-73.9543017,17z/data=!4m5!3m4!1s0x89c244b4f418b1cd:0xb1d4b115f28f8e17!8m2!3d40.6302494!4d-73.9553609">
                 Brooklyn College
               </a>
-            </p>   
+            </p>
+            {/* Countdown Clock */}
+            {/* We should move it down the page somewhere later on */}
+            <Countdown
+              date={new Date('Mar 29, 2020 00:00:00 EST')}
+              renderer={({days, hours, minutes, seconds, completed}) => {
+                if (!completed) {
+                  return <span className="countdown-clock">{days} days, {hours} hours, {minutes} minutes, and {seconds} seconds to go!</span>;
+                } else {
+                  return <span className="countdown-clock">Hack on!</span>;
+                }
+              }}
+            />
           </div>
         
           {/* EMBED GOOGLE MAPS */}

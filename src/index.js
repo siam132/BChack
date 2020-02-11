000ec030b9c60cf54a9e07f0f2e80bf425ee5551
Shahzoda Davlatova <shahzoda01@gmail.com>
2020-02-10 21:53:56 -0500

    moved App.scss, index.scss, and added reset.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './pages/HomePage';
-import './index.scss';
+import './styling/index.scss';
+import './styling/Reset.scss';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));

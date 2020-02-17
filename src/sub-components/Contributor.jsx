d46b2bef998f08b7bf690f62a94a8471604ef5e0
Shahidullah <shahidullah.rahman223@gmail.com>
2020-02-16 21:11:32 -0500

    refractored file names for better readability

--- src/sub-components/Contributor.jsx
+++ src/sub-components/Contributor.jsx
@@ -16,9 +16,8 @@ export default function Contributor(props) {
         <br></br>
         <b className="avatar-role">{props.role}</b>
       </p>
-      {/* <p>Founders of Hack Brooklyn, got together website, sponsors, clubs and everything in the mix.</p> */}
-      <a href="https://www.instagram.com/bc_compsci/"></a>
-      <a href="https://www.facebook.com/groups/bc.cis.club/"></a>
+      
+      
     </div>
   );
 }

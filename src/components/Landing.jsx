1cefe355021f5a6bedbbac4983460b34d9868710
KevinTamCS <kevin@kevintam.dev>
2020-02-17 19:52:22 -0500

    Fix line height

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,38 +1,55 @@
 import React from "react";
-import Zoom from 'react-reveal/Zoom';
-import Countdown from 'react-countdown';
-import "../scss/Landing.scss"
+import Zoom from "react-reveal/Zoom";
+import Countdown from "react-countdown";
+
+import "../scss/Landing.scss";
 
 function Landing() {
   return (
     <div id="landing" className="row">
       <div className="col-12">
-        <Zoom cascade duration={3000}><p className="intro">Where Your Fantasies Come Alive</p></Zoom>
-        <h1 className="glitch">HACK <br /> Brooklyn</h1>
-        <p>March 29th </p>
-        {/* link to location */}
-        <p>
-          <a className="locater" href="https://www.google.com/maps/place/Brooklyn+College/@40.6304648,-73.9543017,17z/data=!4m5!3m4!1s0x89c244b4f418b1cd:0xb1d4b115f28f8e17!8m2!3d40.6302494!4d-73.9553609">
+        <Zoom cascade duration={3000}>
+          <h2 className="intro disable-user-select">Where Your Fantasies Come Alive</h2>
+        </Zoom>
+
+        <h1 className="title disable-user-select">
+          HACK <br /> BROOKLYN
+        </h1>
+
+        <div className="information">
+          <p className="date">March 29th, 2020</p>
+
+          <a
+            className="location"
+            href="https://www.google.com/maps/place/Brooklyn+College/@40.6304648,-73.9543017,17z/data=!4m5!3m4!1s0x89c244b4f418b1cd:0xb1d4b115f28f8e17!8m2!3d40.6302494!4d-73.9553609"
+          >
             Brooklyn College
           </a>
-        </p>
-        {/* Countdown Clock */}
+        </div>
+
+        {/* button to typeform */}
+        <div className="col-12">
+          <button href="#" className="btn register">
+            Register
+          </button>
+        </div>
+
         {/* We should move it down the page somewhere later on */}
         <Countdown
-          date={new Date('Mar 29, 2020 00:00:00 EST')}
+          date={new Date("Mar 29, 2020 00:00:00 EST")}
           renderer={({ days, hours, minutes, seconds, completed }) => {
             if (!completed) {
-              return <span className="countdown-clock">{days} days, {hours} hours, {minutes} minutes, and {seconds} seconds to go!</span>;
+              return (
+                <span className="countdown-clock">
+                  {days} days, {hours} hours, {minutes} minutes, and {seconds}{" "}
+                  seconds to go!
+                </span>
+              );
             } else {
               return <span className="countdown-clock">Hack on!</span>;
             }
           }}
         />
-      </div>
-
-      {/* button to typeform */}
-      <div className="col-12">
-          <button href="#" className="btn register">Register</button>
       </div>
     </div>
   );

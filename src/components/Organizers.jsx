9dc6ec730ef2a0c0af495ebf6ac4693e53791ec0
Shahzoda Davlatova <shahzoda01@gmail.com>
2020-02-21 03:42:31 -0500

    photos linked

--- src/components/Organizers.jsx
+++ src/components/Organizers.jsx
@@ -1,47 +1,48 @@
 import React from "react";
-import { contributors, organizers } from "../data/Contributor_Info";
+import { Contributors, Organizers } from "../data/Contributor_Info";
 import "../scss/Organizers.scss";
 import "../scss/Ground.scss";
 import defaultPic from "../assets/web-dev-pics/test_avatar.png";
+import Shahzoda from "../assets/team-pics/shahzoda.jpeg";
 
-export default function Organizers() {
+export default function Team() {
     return (
-      <div id="organizers">
-        <div className="row organizers-title">
-          <div className="col-12">
+      <div id="organizers" className="row">
+        <div className="col-12 organizers-title">
+          <div>
             <h1>Meet the Organizers!</h1>
           </div>
         </div>
-        <div id="organizers-listings" className="row listings">
-          {organizers.map(person => (
-            <div className="col-lg-3 col-sm-6 avatar-card" align="center">
+        {/* <div id="organizers-listings" className="row listings"> */}
+          {Organizers.map(person => (
+            <div className="col-lg-3 col-sm-4 avatar-card" align="center">
                 <ProfileView person={person} />
             </div>
           ))}
-        </div>
+        {/* </div> */}
 
-        <div className="row organizers-title pt-5">
+        <div className="col-12 organizers-title pt-5">
           <div className="col-12">
             <h1>Meet the Contributors!</h1>
           </div>
         </div>
-        <div id="contributors-listings" className="row listings">
-          {contributors.map(person => (
-            <div className="col-lg-3 col-sm-6 avatar-card" align="center">
+        {/* <div id="contributors-listings" className="row listings"> */}
+          {Contributors.map(person => (
+            <div className="col-lg-3 col-sm-4 avatar-card" align="center">
                 <ProfileView person={person} />
             </div>
           ))}
-        </div>
+        {/* </div> */}
       </div>
     );
 }
 
 export function ProfileView(props) {
-  const { first, last, link, pic, role } = props.person;
+  const { first, last, link, pic } = props.person;
   return (
     <div>
       <img
-        src={pic || defaultPic}
+        src={ require("./../assets" + pic) || defaultPic}
         alt={`${first} ${last}`}
         className="avatar-logo"
       />
@@ -53,9 +54,9 @@ export function ProfileView(props) {
           </a>
         </b>
         <br />
-        <b className="avatar-role">
+        {/* <b className="avatar-role">
           {role}
-        </b>
+        </b> */}
       </p>
     </div>
   );

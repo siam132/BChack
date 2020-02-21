87d498f922e0668c4dd45182f97b99a30f1608c6
Shahzoda Davlatova <shahzoda01@gmail.com>
2020-02-21 05:13:39 -0500

    ground changed to contain instead of cover and removed disable select

--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import "../scss/Ground.scss";
+import Background from "../assets/bottom-ground.svg";
 
 function Ground() {
   return (
-    <div id="ground">
+    <div id="ground" className="row">
+        <img src={ Background } />
     </div>
   );
 }
